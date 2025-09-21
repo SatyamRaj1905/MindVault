5d8aaa982f0fa9b4cab8d8299d0bc494ee061803
SatyamRaj1905 <satyamraj1905@gmail.com>
2025-09-22 01:07:53 +0530

    Parent files and folders added

---
+++ MindVault_fe/src/App.tsx
@@ -0,0 +1,23 @@
+// App.tsx
+
+import { Dashboard } from "./components/pages/Dashboard";
+import { Signin } from "./components/pages/Signin";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup } from "./components/pages/Signup";
+import { TwitterScriptLoader } from "./components/ui/TwitterScriptLoader";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <TwitterScriptLoader />
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
